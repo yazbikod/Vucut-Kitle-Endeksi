708e72562a2ce47881746edc536ed1919ae14065
yazbikod <aslan_murathan@hotmail.com>
2022-10-25 02:11:29 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Vücut Kitle Endeksi Hesaplayıcı");
+Console.WriteLine("Kilonuzu Giriniz : ");
+int kilo = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Boyunuzu Giriniz : ");
+double boy = Convert.ToDouble(Console.ReadLine());
+double endeks = kilo / (boy * boy);
+if (endeks < 18)
+{
+    Console.WriteLine("Zayıfsınız...:/ ");
+}
+else if (endeks >= 18 && endeks <= 25)
+{
+    Console.WriteLine("Normalsiniz...:) ");
+}
+else if (endeks > 25)
+{
+    Console.WriteLine("Obezsiniz... :(");
+}
